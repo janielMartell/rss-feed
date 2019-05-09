5861951c8689cb94e3bb9316e3cd2a4705cc562c
Janiel Martell <martell.janiel@gmail.com>
2019-05-09 00:38:00 -0400

    Add Header

--- src/components/ViewChannelsPage.vue
+++ src/components/ViewChannelsPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div>
+    <header class="bg-logo-blue px-2">
+      <div class="container max-w-lg mx-auto py-2">
+        <div class="flex justify-between items-center">
+          <img class="h-10" src="../assets/bnl-logo.png" alt="BnL Logo">
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
